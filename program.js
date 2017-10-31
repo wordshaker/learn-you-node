15cf85949247208320901ffb92acf2db63846e6e
Jess White <jessica.white@oakbrookfinance.com>
2017-10-31 09:41:33 +0000

    Chapter 2 - Baby Steps (simple version) - complete

--- program.js
+++ program.js
@@ -1,1 +1,10 @@
-console.log("HELLO WORLD");
+console.log(total(process.argv));
+
+function total(arguments)
+{
+    var total = 0;
+    for(i = 2; i < arguments.length; i++){
+        total += Number(arguments[i]);
+    }
+    return total;
+}
